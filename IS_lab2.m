88a98a9b570e3150317456abc51bd471b3bc3366
GabrielaVDD <vdoviak.gabriela@gmail.com>
2021-10-06 18:55:11 +0300

    Fixed code comments

--- IS_lab2.m
+++ IS_lab2.m
@@ -1,17 +1,19 @@
 % IS LD2 
 clear all;
+close all;
 clc;
 %% Main task
-% 1. Duomenų paruošimas
+% Duomenų paruošimas
 x = 0:1/19:1;
 d = ((1 + 0.6*sin(2*pi*x/0.7)) + 0.3*sin(2*pi*x))/2;
 figure(1)
 plot(x, d, 'r+');
 title('Grafikas');
+hold on
 grid on;
 
-% 2. Tinklo struktūros parinkimas
-% paslėptojo sluoksnio svoriai (5 neuronai)
+% Tinklo struktūros parinkimas (pasirinkti 5 neuronai)
+% paslėptojo sluoksnio svoriai
 w11_1 = randn(1);
 w21_1 = randn(1);
 w31_1 = randn(1);
@@ -31,7 +33,7 @@ w14_2 = randn(1);
 w15_2 = randn(1);
 b1_2 = randn(1);
 
-% 3. Tinklo atsako skaičiavimas
+% Tinklo atsako skaičiavimas
 p = 0.2;
 for i = 1:100000
 for n = 1:length(x)
@@ -49,28 +51,28 @@ y4_1 = 1/(1+exp(-v4_1));
 y5_1 = 1/(1+exp(-v5_1));
 % išėjimo sluoksnio neuronui
 v1_2 = y1_1*w11_2 + y2_1*w12_2 + y3_1*w13_2 + y4_1*w14_2 + y5_1*w15_2 + b1_2;
-% atsakas
+% atsakas (tiesinė aktyvavimo funkcija)
 y = v1_2;
 % klaidos skaičiavimas
 e = d(n) - y;
 
-% svorių atnaujinimas
-% klaidos gradiento skaičiavimas išėjimo sluoksnio neuronui
+% Svorių atnaujinimas
+% klaidos gradientas išėjimo sluoksnio neuronui
 delta1_2 = e;
-% klaidos gradiento skaičiavimas paslėpto sluoksnio neuronams
+% klaidos gradientas paslėpto sluoksnio neuronams
 delta1_1 = y1_1*(1-y1_1)*delta1_2*w11_2;
 delta2_1 = y2_1*(1-y2_1)*delta1_2*w12_2;
 delta3_1 = y3_1*(1-y3_1)*delta1_2*w13_2;
 delta4_1 = y4_1*(1-y4_1)*delta1_2*w14_2;
 delta5_1 = y5_1*(1-y5_1)*delta1_2*w15_2;
-% isejimo sluoksnio svoriu atnaujinimas
+% išėjimo sluoksnio svorių atnaujinimas
 w11_2 = w11_2 + p*delta1_2*y1_1;
 w12_2 = w12_2 + p*delta1_2*y2_1;
 w13_2 = w13_2 + p*delta1_2*y3_1;
 w14_2 = w14_2 + p*delta1_2*y4_1;
 w15_2 = w15_2 + p*delta1_2*y5_1;
 b1_2 = b1_2 + p*delta1_2;
-% pasleptojo sluoksnio svoriu atnaujinimas
+% paslėptojo sluoksnio svorių atnaujinimas
 w11_1 = w11_1 + p*delta1_1*x(n);
 w21_1 = w21_1 + p*delta2_1*x(n);
 w31_1 = w31_1 + p*delta3_1*x(n);
@@ -100,12 +102,11 @@ y4_1 = 1/(1+exp(-v4_1));
 y5_1 = 1/(1+exp(-v5_1));
 % išėjimo sluoksnio neuronui
 v1_2 = y1_1*w11_2 + y2_1*w12_2 + y3_1*w13_2 + y4_1*w14_2 + y5_1*w15_2 + b1_2;
-% aktyvavimo funkcija (tiesinė)
-y1_2 = v1_2;
-% atsakas
-y = y1_2;
-% tarpines reiksmes
+% aktyvavimo funkcija (tiesinė aktyvavimo funkcija)
+y = v1_2;
 Y(n) = y;
 end
-hold on
-plot(x, Y, 'bo')
+plot(x, Y, 'b+')
+legend('Norimas atsakas','Gautas atsakas');
+
+% PAPILDOMA UŽDUOTIS
